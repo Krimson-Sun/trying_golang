d8b12910bc5e85d7241721017ce95a9a8be0597f
Krimson-Sun <gamesdi@mail.ru>
2024-04-03 00:52:49 +0300

    pg transaction logger

--- dblogger.go
+++ dblogger.go
@@ -8,14 +8,14 @@ import (
 )
 
 type PgTransactionLogger struct {
-	event  chan<- Event
+	events chan<- Event
 	errors <-chan error
 	db     *sql.DB
 	wg     *sync.WaitGroup
 }
 
 func (l *PgTransactionLogger) WritePut(key, value string) {
-	l.event <- Event{
+	l.events <- Event{
 		EventType: EventPut,
 		key:       key,
 		value:     value,
@@ -23,7 +23,7 @@ func (l *PgTransactionLogger) WritePut(key, value string) {
 }
 
 func (l *PgTransactionLogger) WriteDelete(key string) {
-	l.event <- Event{
+	l.events <- Event{
 		EventType: EventDelete,
 		key:       key,
 	}
@@ -88,10 +88,10 @@ func (l *PgTransactionLogger) verifyTableExists() (bool, error) {
 func (l *PgTransactionLogger) createTable() error {
 	var err error
 	_, err = l.db.Exec(`CREATE TABLE transactions (
-		sequence BIGSERIAL PRIMARY KEY,
-		even_type BYTE NOT NULL,
-		key TEXT NOT NULL,
-		value TEXT
+		sequence 	BIGSERIAL PRIMARY KEY,
+		even_type 	BYTE NOT NULL,
+		key 		TEXT NOT NULL,
+		value 		TEXT
 	);`)
 	if err != nil {
 		return err
@@ -101,9 +101,55 @@ func (l *PgTransactionLogger) createTable() error {
 }
 
 func (l *PgTransactionLogger) Run() {
-	// TODO
+	events := make(chan Event)
+	l.events = events
+	errors := make(chan error, 1)
+	l.errors = errors
+
+	go func() {
+		query := `INSERT INTO transactions (event_type, key, value) VALUES ($1, $2, $3)`
+
+		for event := range events {
+			_, err := l.db.Exec(query, event.EventType, event.key, event.value)
+			if err != nil {
+				errors <- err
+			}
+			l.wg.Done()
+		}
+	}()
 }
 
 func (l *PgTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
-	return nil, nil // TODO
+	outEvent := make(chan Event)
+	outError := make(chan error)
+
+	go func() {
+		defer close(outEvent)
+		defer close(outError)
+
+		rows, err := l.db.Query(`SELECT sequence, event_type, key, value FROM transactions ORDER BY sequence`)
+		if err != nil {
+			outError <- err
+			return
+		}
+
+		defer rows.Close()
+
+		e := Event{}
+
+		for rows.Next() {
+			err = rows.Scan(&e.Sequence, &e.EventType, &e.key, &e.value)
+
+			if err != nil {
+				outError <- fmt.Errorf("error reading from log file: %w", err)
+			}
+			outEvent <- e
+		}
+
+		err = rows.Err()
+		if err != nil {
+			outError <- fmt.Errorf("error reading from log file: %w", err)
+		}
+	}()
+	return outEvent, outError
 }
